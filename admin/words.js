d95ef47e381ccdb6915a60bad2134398807c2424
Christian Biermann <chrbier@googlemail.com>
2021-12-27 16:45:45 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "spanet adapter settings": {
+        "en": "Adapter settings for spanet",
+        "de": "Adaptereinstellungen für spanet",
+        "ru": "Настройки адаптера для spanet",
+        "pt": "Configurações do adaptador para spanet",
+        "nl": "Adapterinstellingen voor spanet",
+        "fr": "Paramètres d'adaptateur pour spanet",
+        "it": "Impostazioni dell'adattatore per spanet",
+        "es": "Ajustes del adaptador para spanet",
+        "pl": "Ustawienia adaptera dla spanet",
+        "zh-cn": "spanet的适配器设置"
+    },
+    "option1": {
+        "en": "option1",
+        "de": "option1",
+        "ru": "option1",
+        "pt": "option1",
+        "nl": "option1",
+        "fr": "option1",
+        "it": "option1",
+        "es": "option1",
+        "pl": "option1",
+        "zh-cn": "option1"
+    },
+    "option2": {
+        "en": "option2",
+        "de": "option2",
+        "ru": "option2",
+        "pt": "option2",
+        "nl": "option2",
+        "fr": "option2",
+        "it": "option2",
+        "es": "option2",
+        "pl": "option2",
+        "zh-cn": "option2"
+    }
+};
